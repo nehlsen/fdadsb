cd3f54fd505b328c7b1d2ac3cb7b17aab2feba95
Nils Weigel <nils.weigel@gmail.com>
2015-11-29 10:15:22 +0100

    initial work on input-step filter

--- src/Fda/TournamentBundle/Engine/Setup/InputStep.php
+++ src/Fda/TournamentBundle/Engine/Setup/InputStep.php
@@ -49,7 +49,46 @@ class InputStep implements InputInterface
      */
     public function filter(RoundGearsInterface $previousRoundGears)
     {
+        $advance = $previousRoundGears->getRound()->getSetup()->getAdvance(); // -1 = all
+        $leaderBoard = $previousRoundGears->getLeaderBoard();
+
+        $result = array();
+        $groupNumbers = $leaderBoard->getGroupNumbers();
+        foreach ($groupNumbers as $groupNumber) {
+            $result[$groupNumber] = array();
+        }
+
+        $groupNumberStepped = function ($step) use ($groupNumbers) {
+            $it = new \ArrayIterator($groupNumbers);
+            $it->seek($step % $it->count());
+            return $it->current();
+        };
+
+        $players = $leaderBoard->getEntries($advance);
+
+        $step = 0;
+        foreach ($groupNumbers as $groupNumber) {
+            $groupAdvance = -1 == $advance ? count($players[$groupNumber]) : $advance;
+            for ($i = 0; $i < $groupAdvance; ++$i) {
+                $result[$groupNumberStepped($step+$i)][] = $players[$groupNumber][$i]->getPlayer();
+            }
+
+            $step += $this->getSteps();
+        }
+//        $step = 0;
+//        for ($i = 0; $i < $advance; ++$i) {
+//            foreach ($groupNumbers as $groupNumber) {
+//                $result[$groupNumberStepped($step+1)][] = $players[$groupNumber][$i];
+//            }
+//
+//            $step += $this->getSteps();
+//        }
+
+//        dump($players, $result);
+
         // TODO implement input step : filter
-        throw new \Exception('TODO');
+//        throw new \Exception('TODO');
+
+        return $result;
     }
 }
