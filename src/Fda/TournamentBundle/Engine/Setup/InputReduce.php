590dfcc79c51a1978c20822af2ed1ef6ddf475e8
Nils Weigel <nils.weigel@gmail.com>
2015-11-29 12:44:05 +0100

    implemented reduce filter

--- src/Fda/TournamentBundle/Engine/Setup/InputReduce.php
+++ src/Fda/TournamentBundle/Engine/Setup/InputReduce.php
@@ -3,6 +3,7 @@
 namespace Fda\TournamentBundle\Engine\Setup;
 
 use Fda\TournamentBundle\Engine\Gears\RoundGearsInterface;
+use Fda\TournamentBundle\Engine\LeaderBoard\LeaderBoardEntryInterface;
 
 class InputReduce implements InputInterface
 {
@@ -19,7 +20,36 @@ class InputReduce implements InputInterface
      */
     public function filter(RoundGearsInterface $previousRoundGears)
     {
-        // TODO implement input reduce : filter
-        throw new \Exception('TODO');
+        $leaderBoard = $previousRoundGears->getLeaderBoard();
+
+        if (count($leaderBoard->getGroupNumbers()) % 2 != 0) {
+            throw new \RuntimeException(
+                'input reduce cuts the number of groups in half and can therefore only work with even group numbers.'
+            );
+        }
+
+        $result = array();
+        $newGroupCount = count($leaderBoard->getGroupNumbers())/2;
+        for($n = 0; $n < $newGroupCount; ++$n) {
+            $result[] = array();
+        }
+
+        // foreach group
+        //  take first player and put into group%2
+        //  repeat ... -> advance
+
+        $remapGroupNumber = function ($groupNumber) use ($newGroupCount) {
+            return $groupNumber%$newGroupCount;
+        };
+
+        $advance = $previousRoundGears->getRound()->getSetup()->getAdvance(); // -1 = all
+        foreach ($leaderBoard->getEntries($advance) as $groupNumber => $groupedEntries) {
+            /** @var LeaderBoardEntryInterface[] $groupedEntries */
+            foreach ($groupedEntries as $entry) {
+                $result[$remapGroupNumber($groupNumber)][] = $entry->getPlayer();
+            }
+        }
+
+        return $result;
     }
 }
